8da298eba4ab2a111d9596bd76f7d83ec79ce35d
simo <svm.zhang@gmail.com>
2024-09-28 22:35:57 -0700

    initial commit; definition of CLI

---
+++ src/pyhlatyper/cli.py
@@ -0,0 +1,50 @@
+import argparse
+
+from libscibio import parse_path
+
+
+def parse_cmd() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--bam",
+        metavar="FILE",
+        type=parse_path,
+        required=True,
+        help="specify path to BAM file",
+    )
+    parser.add_argument(
+        "--freq",
+        metavar="FILE",
+        type=parse_path,
+        required=True,
+        help="specify path to HLA frequency file",
+    )
+    parser.add_argument(
+        "--race",
+        metavar="STR",
+        type=str,
+        default="Unknown",
+        help="specify race [default: Unknown]",
+    )
+    parser.add_argument(
+        "--out",
+        metavar="FILE",
+        type=parse_path,
+        required=True,
+        help="specify path to output hlatyping result file",
+    )
+    parser.add_argument(
+        "--min_ecnt",
+        metavar="INT",
+        type=int,
+        default=999,
+        help="specify minimum # of mm events (999)",
+    )
+    parser.add_argument(
+        "--nproc",
+        metavar="INT",
+        type=int,
+        default=8,
+        help="specify # processes to use (8)",
+    )
+    return parser
