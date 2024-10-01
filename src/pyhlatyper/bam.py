61df6e2a0acb3492092a2024f7f5b2b7f7f05029
simo <svm.zhang@gmail.com>
2024-09-30 18:06:49 -0700

    initial commit; module dealing with BAM file

---
+++ src/pyhlatyper/bam.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+import pysam
+from libscibio import parse_path
+
+
+@dataclass
+class BAMetadata:
+    fspath: str
+    sort_by: str = field(init=False, default="")
+    references: list[dict[str, int]] = field(init=False, default_factory=list)
+    read_groups: list[dict[str, str]] = field(init=False, default_factory=list)
+
+    def __post_init__(self) -> None:
+        """Parse metadata out of given BAM file"""
+        with pysam.AlignmentFile(self.fspath, "rb") as bamf:
+            header = bamf.header.to_dict()
+            self._parse_read_groups(header)
+            self._parse_sort_by(header)
+            self._parse_references(header)
+
+    def _parse_read_groups(self, header: dict[str, Any]) -> None:
+        """Parse read groups from the header"""
+        self.read_groups = header.get("RG", [])
+
+    def _parse_sort_by(self, header: dict[str, Any]) -> None:
+        """Parse sort_by information from the header"""
+        hd = header.get("HD", {})
+        self.sort_by = hd.get("SO", "")
+        if self.sort_by == "coordinate":
+            self._check_bai()
+
+    def _parse_references(self, header: dict[str, Any]) -> None:
+        """Parse references from the header"""
+        sqs = header.get("SQ", [])
+        if not sqs:
+            raise IndexError("No sequence information found in the header")
+
+        self.references = [
+            {s["SN"]: int(s["LN"])} for s in sqs if "SN" in s and "LN" in s
+        ]
+
+    def _check_bai(self) -> None:
+        """Check if index file exists for the coordinate-sorted BAM"""
+        bam = parse_path(self.fspath)
+        bai = bam.parent / f"{bam.name}.bai"
+        if not bai.exists():
+            bai = bam.with_suffix(".bai")
+            if not bai.exists():
+                raise FileNotFoundError(
+                    f"Cannot find the index file for the given BAM {bam}"
+                )
+
+    def __repr__(self) -> str:
+        """Return BAMetadata object representation"""
+        return (
+            f"BAM file: {self.fspath}\n"
+            f"Sort by: {self.sort_by}\n"
+            f"# references: {len(self.references)}\n"
+            f"# read groups: {len(self.read_groups)}\n"
+        )
+
+    def seqnames(self) -> list[str]:
+        return [k for r in self.references for k in r.keys()]
