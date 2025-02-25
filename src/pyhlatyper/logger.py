a0fddf822fd0a0a95afe903069ff1f9b7dc732cf
simo <svm.zhang@gmail.com>
2025-02-25 10:31:43 -0800

    Added file handler to logger when debug mode is on.

--- src/pyhlatyper/logger.py
+++ src/pyhlatyper/logger.py
@@ -1,5 +1,14 @@
+from __future__ import annotations
+
 import logging
-from typing import cast
+from typing import TYPE_CHECKING, cast
+
+from tqdm.auto import tqdm
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+    from tinyscibio import _PathLike
 
 
 class Logger(logging.Logger):
@@ -7,20 +16,35 @@ class Logger(logging.Logger):
         super().__init__(name)
         self._configured = False
 
-    def initialize(self, debug: bool = False) -> None:
+    def initialize(
+        self, debug: bool = False, f: Optional[_PathLike] = None
+    ) -> None:
         if self._configured:
             return
 
         self.setLevel(logging.DEBUG if debug else logging.INFO)
+        if self.hasHandlers():
+            self.handlers.clear()
 
-        # Create console handler with specific format
-        if not self.hasHandlers():
-            console_handler = logging.StreamHandler()
-            formatter = logging.Formatter(
-                "[%(module)s.py] - [%(funcName)s:%(lineno)d] - [%(levelname)s]: %(message)s"
-            )
-            console_handler.setFormatter(formatter)
-            self.addHandler(console_handler)
+        console_handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "[%(module)s.py] - [%(funcName)s:%(lineno)d] - [%(levelname)s]: %(message)s"
+        )
+        console_handler.setFormatter(formatter)
+        console_handler.setLevel(logging.INFO)
+        self.addHandler(console_handler)
+
+        if debug:
+            if f is None:
+                raise ValueError(
+                    f"Parameter {f=} is required to add a FileHandler to "
+                    "logger when debug is true."
+                )
+            file_handler = logging.FileHandler(f, mode="a")
+            file_handler.setFormatter(formatter)
+            file_handler.setLevel(logging.DEBUG)
+            self.addHandler(file_handler)
+
         self._configured = True
 
 
