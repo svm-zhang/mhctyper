0e002f61fb743e29eef295a62d9d9a1fde1fdb4e
simo <svm.zhang@gmail.com>
2025-03-17 08:40:18 -0700

    Added mhctyper_main.

--- src/mhctyper/__init__.py
+++ src/mhctyper/__init__.py
@@ -1,5 +1,5 @@
-from mhctyper.mhctyper import run_mhctyper
+from mhctyper.mhctyper import mhctyper_main, run_mhctyper
 
 from ._version import version as __version__
 
-__all__ = ["run_mhctyper", "__version__"]
+__all__ = ["mhctyper_main", "run_mhctyper", "__version__"]
